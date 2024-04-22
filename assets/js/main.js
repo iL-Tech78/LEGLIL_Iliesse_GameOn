fe29c194ffcf5b74b2dd5760365e9affdbee461a
IL-iliesse78 <86209795+IL-iliesse78@users.noreply.github.com>
2024-04-22 09:41:08 +0200

    - Amélioration de l'architecture du code - Refonte complète du code HTML, CSS et JavaScript pour le header et la modale. - Ajout de la fonction de fermeture de la modale pour une meilleure expérience utilisateur.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,53 @@
-function editNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+// function editNav() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+  
+//   // DOM Elements
+//   const modalbg = document.querySelector(".bground");
+//   const modalBtn = document.querySelectorAll(".modal-btn");
+//   const formData = document.querySelectorAll(".formData");
+  
+//   // launch modal event
+//   modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+  
+//   // launch modal form
+//   function launchModal() {
+//     modalbg.style.display = "block";
+//   }
+  
+  
+/*=============== REFONTE NAV ===============*/
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener('click', ()=>{
+  navbar.classList.toggle('active');
+  menu.classList.toggle('style-menu');
+})
+
+// /*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) =>{
+  const openBtn = document.getElementById(openButton),
+  modalContainer = document.getElementById(modalContent)
+  
+  if(openBtn && modalContainer){
+      openBtn.addEventListener('click', ()=>{
+          modalContainer.classList.add('show-modal')
+      })
   }
-  
-  // DOM Elements
-  const modalbg = document.querySelector(".bground");
-  const modalBtn = document.querySelectorAll(".modal-btn");
-  const formData = document.querySelectorAll(".formData");
-  
-  // launch modal event
-  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-  
-  // launch modal form
-  function launchModal() {
-    modalbg.style.display = "block";
-  }
-  
-  
-  
+}
+showModal('open-modal','modal-container')
+
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+  const modalContainer = document.getElementById('modal-container')
+  modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(close => close.addEventListener('click', closeModal))
