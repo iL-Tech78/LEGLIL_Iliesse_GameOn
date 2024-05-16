ac8cceecbf8b493a162b3b4de6a6d4385224b13d
IL-iliesse78 <86209795+IL-iliesse78@users.noreply.github.com>
2024-05-16 15:50:57 +0200

    Ajout de la validation visuelle des champs d'entrée avec bordure rouge pour les saisies invalides.

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,82 +32,89 @@ closeBtn.forEach(close => close.addEventListener('click', closeModal))
 /*=============== FORMULAIRE ===============*/
 
 document.getElementById("formGameOnForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // J'empêche le formulaire de se soumettre normalement
+    event.preventDefault(); // J'empêche le formulaire de se soumettre normalement
 
-  // Initialisation du tableau des erreurs
-  const errors = [];
+    // Initialisation du tableau des erreurs
+    const errors = [];
 
-  // Récupération des valeurs des champs du formulaire
-  const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  const email = document.getElementById("email").value;
-  const numTournaments = document.getElementById("numTournaments").value;
-  const tournamentChoice = document.querySelector('input[name="tournamentChoice"]:checked');
-  const termsConditions = document.querySelector('input[name="termsConditions"]').checked;
-  const btnFermer = document.querySelector('btn-fermer');
+    // Récupération des valeurs des champs du formulaire
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const email = document.getElementById("email");
+    const numTournaments = document.getElementById("numTournaments");
+    const tournamentChoice = document.querySelector('input[name="tournamentChoice"]:checked');
+    const termsConditions = document.querySelector('input[name="termsConditions"]').checked;
 
-  // Fonction de validation de l'adresse email
-  function validateEmail(email) {
-      const re = /\S+@\S+\.\S+/;
-      return re.test(email);
-  }
+    // Fonction de validation de l'adresse email
+    function validateEmail(email) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
 
-  // Fonction de validation du nombre de tournois
-  function validateNumTournaments(numTournaments) {
-      return !isNaN(numTournaments);
-  }
+    // Fonction de validation du nombre de tournois
+    function validateNumTournaments(numTournaments) {
+        return !isNaN(numTournaments);
+    }
 
-  // Validation des champs du formulaire 
-  if (firstName.length < 2) {
-      errors.push("Le prénom doit comporter au moins 2 caractères.");
-      document.getElementById("firstNameError").textContent = errors[errors.length - 1];
-  } else {
-      document.getElementById("firstNameError").textContent = "";
-  }
+    // Supprimer toutes les bordures rouges
+    const allInputs = document.querySelectorAll("input");
+    allInputs.forEach(input => input.classList.remove("invalid"));
 
-  if (lastName.length < 2) {
-      errors.push("Le nom de famille doit comporter au moins 2 caractères.");
-      document.getElementById("lastNameError").textContent = errors[errors.length - 1];
-  } else {
-      document.getElementById("lastNameError").textContent = "";
-  }
+    // Validation des champs du formulaire 
+    if (firstName.value.length < 2) {
+        errors.push("Le prénom doit comporter au moins 2 caractères.");
+        document.getElementById("firstNameError").textContent = errors[errors.length - 1];
+        firstName.classList.add("invalid");
+    } else {
+        document.getElementById("firstNameError").textContent = "";
+    }
 
-  if (!validateEmail(email)) {
-      errors.push("L'adresse électronique n'est pas valide.");
-      document.getElementById("emailError").textContent = errors[errors.length - 1];
-  } else {
-      document.getElementById("emailError").textContent = "";
-  }
+    if (lastName.value.length < 2) {
+        errors.push("Le nom de famille doit comporter au moins 2 caractères.");
+        document.getElementById("lastNameError").textContent = errors[errors.length - 1];
+        lastName.classList.add("invalid");
+    } else {
+        document.getElementById("lastNameError").textContent = "";
+    }
 
-  if (!numTournaments.trim() || isNaN(numTournaments) || numTournaments < 0) {
-    document.getElementById("numTournamentsError").textContent = "Veuillez saisir un nombre supérieur ou égal à 0 pour le nombre de concours.";
-  } else {
-      document.getElementById("numTournamentsError").textContent = "";
-  }
+    if (!validateEmail(email.value)) {
+        errors.push("L'adresse électronique n'est pas valide.");
+        document.getElementById("emailError").textContent = errors[errors.length - 1];
+        email.classList.add("invalid");
+    } else {
+        document.getElementById("emailError").textContent = "";
+    }
 
-  if (!tournamentChoice) {
-      errors.push("Veuillez sélectionner un tournoi.");
-      document.getElementById("tournamentChoiceError").textContent = errors[errors.length - 1];
-  } else {
-      document.getElementById("tournamentChoiceError").textContent = "";
-  }
+    if (!numTournaments.value.trim() || isNaN(numTournaments.value) || numTournaments.value < 0) {
+        document.getElementById("numTournamentsError").textContent = "Veuillez saisir un nombre supérieur ou égal à 0 pour le nombre de concours.";
+        numTournaments.classList.add("invalid");
+    } else {
+        document.getElementById("numTournamentsError").textContent = "";
+    }
 
-  if (!termsConditions) {
-    errors.push("Vous devez accepter les conditions générales pour continuer.");
-    document.getElementById("termsConditionsError").textContent = errors[errors.length - 1];
-  } else {
-    document.getElementById("termsConditionsError").textContent = "";
-}
+    if (!tournamentChoice) {
+        errors.push("Veuillez sélectionner un tournoi.");
+        document.getElementById("tournamentChoiceError").textContent = errors[errors.length - 1];
+    } else {
+        document.getElementById("tournamentChoiceError").textContent = "";
+    }
 
-  // Affichage des erreurs ou soumission du formulaire
-  if (errors.length > 0) {
-      console.log("Erreurs de validation :", errors);
-  } else {
-      // Si aucune erreur, afficher le message de confirmation
-      document.querySelector("#formGameOnForm").style.display = "none";
-      document.getElementById("confirmationMessage").textContent = "Merci pour votre inscription";
-      document.getElementById("confirmationMessage").style.display = "flex";
-      document.getElementById("btnFermer").classList.remove('d-n');
-    //   btnFermer.classList.add('d-b');
-  }
+    if (!termsConditions) {
+        errors.push("Vous devez accepter les conditions générales pour continuer.");
+        document.getElementById("termsConditionsError").textContent = errors[errors.length - 1];
+    } else {
+        document.getElementById("termsConditionsError").textContent = "";
+    }
+
+    // Affichage des erreurs ou soumission du formulaire
+    if (errors.length > 0) {
+        console.log("Erreurs de validation :", errors);
+    } else {
+        // Si aucune erreur, afficher le message de confirmation
+        document.querySelector("#formGameOnForm").style.display = "none";
+        document.getElementById("confirmationMessage").textContent = "Merci pour votre inscription";
+        document.getElementById("confirmationMessage").style.display = "flex";
+        document.getElementById("btnFermer").classList.remove('d-n');
+    }
 });
+
